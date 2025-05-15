782736ab0cecb0012c8180cadc36d7eac500de60
Skitlovesz <segredodorapdofunk@gmail.com>
2025-05-15 16:38:39 -0300

    fix: cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { OK, NOT_FOUND } = require("./helpers/statusCodes");
 
 // Cors
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header('Access-Control-Allow-Origin', '*')
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     if (req.method === "OPTIONS") {
